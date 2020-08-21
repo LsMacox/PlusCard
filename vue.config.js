5f67db269d394d840ec78b00f87719792eed42f0
DmitriyAnokhin <hunter9@bk.ru>
2020-08-21 11:32:52 +0700

    auth forms

--- vue.config.js
+++ vue.config.js
@@ -11,7 +11,7 @@ const port = process.env.port || process.env.npm_config_port || 9527 // dev port
 
 module.exports = {
   publicPath: '/',
-  outputDir: 'dist',
+  outputDir: 'public',
   assetsDir: 'static',
   lintOnSave: process.env.NODE_ENV === 'development',
   devServer: {
