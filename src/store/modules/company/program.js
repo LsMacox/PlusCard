2e3dba17ce632844c596706ad290210d029eee8f
DmitriyAnokhin <hunter9@bk.ru>
2020-08-25 10:56:00 +0700

    store

--- src/store/modules/company/program.js
+++ src/store/modules/company/program.js
@@ -1,30 +1,34 @@
-
 const getDefaultState = () => {
   return {
     programs: [], // компании продавца
+    program: {},
   }
 }
 
-// initial state
 const state = getDefaultState()
 
 const mutations = {
-  RESET_STATE (state) {
-    Object.assign(state, getDefaultState())
-  },
+  RESET_STATE: (state) => Object.assign(state, getDefaultState()),
   SET_PROGRAMS (state, payload) {
     state.programs = payload
+  },
+  SET_PROGRAM (state, payload) {
+    state.program = payload
   },
 }
 
 const actions = {
-  Reset ({ commit }) {
+  resetState ({ commit }) {
     commit('RESET_STATE')
   },
-  changeDrawer ({ commit }, value) {
+  list ({ commit }, value) {
     commit('SET_DRAWER', value)
   },
+}
 
+const getters = {
+  programs: state => state.programs,
+  program: state => state.program,
 }
 
 export default {
@@ -32,4 +36,5 @@ export default {
   state,
   mutations,
   actions,
+  getters,
 }
