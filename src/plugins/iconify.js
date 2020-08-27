d493741b66872886acdd65e62be3b73ff6c7e614
DmitriyAnokhin <hunter9@bk.ru>
2020-08-27 14:11:50 +0700

    filter

--- src/plugins/iconify.js
+++ src/plugins/iconify.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import IconifyIcon from '@iconify/vue'
 
-//https://iconify.design/icon-sets/?query=plus-circle
+// https://iconify.design/icon-sets/?query=plus-circle
+// npm i @iconify/icons-bx -D
+// npm i @iconify/icons-codicon -D
 export const ICONIFY_ICONS = [
   { id: 'plus-circle', component: () => import('@iconify/icons-uil/plus-circle') },
   { id: 'plus-circle-outlined', component: () => import('@iconify/icons-ant-design/plus-circle-outlined') },
+  { id: 'bx-check', component: () => import('@iconify/icons-bx/bx-check') },
+  { id: 'chrome-close', component: () => import('@iconify/icons-codicon/chrome-close') },
 ]
 
 ICONIFY_ICONS.forEach((icon) => {
--- src/views/loyalty/report/table/Toolbar.vue
+++ src/views/loyalty/report/table/Toolbar.vue
@@ -1,16 +1,14 @@
 <template>
-  <v-form>
-    <v-container>
-      <v-row
-        no-gutters
-      >
-        <v-col
-          cols="6"
-          style="padding-right: 20px"
-        >
+  <v-container>
+    <v-row
+      no-gutters
+    >
+      <v-col>
+        <div class="app__popover">
           <v-text-field
             outlined
-            placeholder="Поиск операций, валют, клиентов"
+            placeholder="Поиск и фильтр"
+            @focus="switchShow"
           >
             <template v-slot:prepend-inner>
               <span
@@ -20,35 +18,82 @@
               />
             </template>
           </v-text-field>
-        </v-col>
-
-        <v-col
-          cols="5"
-          style="padding-right: 20px"
-        >
-          <v-select
-            outlined
-            placeholder="Бонусная операция"
-          />
-        </v-col>
-
-        <v-col cols="1">
-          <v-chip
-            class="advanced-filter"
+          <div
+            v-show="show"
+            class="app__popover-block"
           >
-            <span
-              class="iconify"
-              data-icon="fa:sliders"
-              data-inline="false"
-            />
-            <span>
-              Все фильтры
-            </span>
-          </v-chip>
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-form>
+            <v-text-field
+              ref="search"
+              outlined
+              placeholder="Поиск и фильтр"
+            >
+              <template v-slot:prepend-inner>
+                <span
+                  class="iconify"
+                  data-icon="ant-design:search-outlined"
+                  data-inline="false"
+                />
+              </template>
+              <template v-slot:append>
+                <v-icon
+                  class="app__popover-block-icon-close"
+                  @click="close()"
+                >
+                  $iconify_chrome-close
+                </v-icon>
+                <v-icon
+                  class="app__popover-block-icon-check"
+                  @click="apply()"
+                >
+                  $iconify_bx-check
+                </v-icon>
+              </template>
+            </v-text-field>
+            <div class="app__popover-content">
+              <v-row>
+                <v-col>
+                  <div class="app__popover-content-header">
+                    Операции
+                  </div>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col>
+                  <div class="app__popover-content-header">
+                    Валюта
+                  </div>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col>
+                  <div class="app__popover-content-header">
+                    Операторы
+                  </div>
+                  <div>
+                    <input
+                      class="app__popover-content-input"
+                      placeholder="Начните вводить имя оператора"
+                    >
+                  </div>
+                </v-col>
+                <v-col>
+                  <div class="app__popover-content-header">
+                    Клиенты
+                  </div>
+                  <div>
+                    <input
+                      class="app__popover-content-input"
+                      placeholder="Начните вводить имя клиента"
+                    >
+                  </div>
+                </v-col>
+              </v-row>
+            </div>
+          </div>
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -57,20 +102,76 @@
     },
     data () {
       return {
+        show: false,
       }
     },
     methods: {
-
+      async switchShow () {
+        this.show = !this.show
+        await this.$nextTick()
+        this.$refs.search.focus()
+      },
+      close () {
+        this.show = false
+      },
+      apply () {
+        this.show = false
+      },
     },
   }
 </script>
 
 <style lang="sass" scoped>
-.advanced-filter
-  //font-style: normal
-  //font-weight: 600
-  //font-size: 13px
-  //line-height: 17px
-  color: #4776E6
+.app__popover
+  position: relative
+
+  .app__popover-block
+    position: absolute
+    top: 0
+    left: 0
+    width: 100%
+    background: #FFFFFF
+    border: 1px solid #E8E8ED
+    box-sizing: border-box
+    box-shadow: 0px 12px 24px rgba(88, 93, 106, 0.1)
+    border-radius: 10px
+    z-index: 1000
+
+    .app__popover-content
+      padding: 20px 20px 8px 20px
+
+      .app__popover-content-header
+        margin-bottom: 12px
+        font-style: normal
+        font-weight: 600
+        font-size: 15px
+        line-height: 21px
+        letter-spacing: 0.1px
+        color: #2A2A34
+
+      .app__popover-content-input
+        width: 100%
+        outline: none !important
+        padding: 11px 12px
+        font-style: normal
+        font-weight: 500
+        font-size: 13px
+        line-height: 17px
+        color: #9191A1
+        border: 1px solid #D7D7E0
+        box-sizing: border-box
+        border-radius: 10px
+
+.app__popover-block-icon-close
+  color: #B5B5C4 !important
+  cursor: pointer
+
+.app__popover-block-icon-check
+  width: 42px !important
+  height: 28px !important
+  position: relative
+  top: -4px
+  margin-left: 5px
+  color: #4776E6 !important
   cursor: pointer
 </style>
