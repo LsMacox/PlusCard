21035bd0f530869aa2fda24e0c47f4011f0da746
Legato <legatokun@gmail.com>
2020-09-07 17:21:04 +0700

    user cert header

--- src/views/account/certificate/table/index.vue
+++ src/views/account/certificate/table/index.vue
@@ -1,18 +1,35 @@
 <template>
-  <v-container>
-    <certificate-filter />
-    <certificate-table />
-  </v-container>
+  <v-row no-gutters>
+    <v-col>
+    <v-row no-gutters style="margin-bottom: 34px;">
+      <v-col>
+         <certificate-header  />
+      </v-col>
+     
+    </v-row>
+     <v-row>
+       <v-col><certificate-filter /></v-col>
+      
+    </v-row>
+     <v-row> <v-col>
+      <certificate-table />
+      </v-col>
+    </v-row>
+    
+   </v-col>
+  </v-row>
 </template>
 
 <script>
   import CertificateTable from './CertificateTable'
   import CertificateFilter from './Filter'
+  import CertificateHeader from './Header'
 
   export default {
     components: {
       CertificateFilter,
       CertificateTable,
+      CertificateHeader
     },
     props: {
     },
