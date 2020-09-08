8519e5cdd4b54da66701f4da5aeb9e0b4ae64778
DmitriyAnokhin <hunter9@bk.ru>
2020-09-08 17:13:42 +0700

    table fix

--- src/views/loyalty/report/table/Table.vue
+++ src/views/loyalty/report/table/Table.vue
@@ -90,7 +90,7 @@
             <div style="display: flex; align-items: center">
               <img
                 class="cell-avatar"
-                :style="`background-color: ${program.bgcolor2};`"
+                :style="`background-color: ${program.bgcolor1};`"
                 :src="(item.operator_avatar === 'system' || item.operator_avatar === 'external')
                   ? program.logo_short
                   : `https://storage.yandexcloud.net/plusstorage/${item.operator_avatar}`"
