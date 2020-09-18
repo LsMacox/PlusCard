b9264b9d4b6781dfb4ce8ff08bd54362901c3eca
Legato <legatokun@gmail.com>
2020-09-18 18:11:07 +0700

    Экран витрины сертификатов программы: * общий компонент * блок сертифката

--- src/views/certificate/ProgramCertificateView.vue
+++ src/views/certificate/ProgramCertificateView.vue
@@ -1,19 +1,42 @@
 <template lang="">
-  <div>
-    {{ program }} <v-btn
-      color="primary"
-      dark
-      @click="onMasterCreateCert"
-    >
-      Выпустить сертификат
-    </v-btn>
-  </div>
+  <v-container
+    fluid
+    class="program-settings"
+  >
+    <v-row>
+      <v-col class="title-m-bold neutral-900--text">
+        Настройка сертификатов
+      </v-col>
+      <v-spaser />
+      <v-col>
+        <v-row>
+        <base-text-field
+          v-model="search"
+          :prepend-inner-icon="'$iconify_search-outlined'"
+          placeholder="Поиск сертификатов"
+        />
+        <v-btn color="primary"> <v-icon left>fa-plus</v-icon> Создать новый сертификат</v-btn>
+         </v-row>
+      </v-col>
+
+
+    </v-row>
+    <v-row>
+      <v-col>
+        <program-certificate-card :name="'sdf'" />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
   export default {
+    components: {
+      ProgramCertificateCard: () => import('./components/ProgramCertificateCard'),
+    },
     data () {
       return {
+        search: '',
 
       }
     },
--- src/views/certificate/components/ProgramCertificateCard.vue
+++ src/views/certificate/components/ProgramCertificateCard.vue
@@ -1,18 +1,68 @@
 <template lang="">
-  <div> {{ program }} </div>
-</template>
+  <v-container
+    fluid
+    class="program-cert-block"
+    @click="blockClick()"
+  >
+    <v-row>
+      <v-col>
+        <span>{{name}}</span>
+        <v-btn>Status</v-btn>
+      </v-col>
+      <v-spaser />
+      <v-col>
+        <v-btn> <v-icon>fa-plus</v-icon> </v-btn>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>{{name}}</v-col>
+    </v-row>
+    <v-row>
+      <v-col>  <v-switch
+      v-model="active"
+      label="Опубликован"
+    ></v-switch></v-col>
+    </v-row>
+    <v-row>
+      <v-col>        
+        <v-chip label close>sdfsdf</v-chip>
+        <v-btn>plus</v-btn>
 
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
 <script>
   export default {
-    data () {
-      return {
-
-      }
-    },
-    computed: {
-      program () {
-        return { id: 1 }
+    props: {
+      name: {
+        type: String,
+        default: '',
       },
+      active: {
+        type: Boolean,
+        default: false,
+      },      
     },
+    methods: {
+      
+    },
+
   }
 </script>
+
+<style lang="scss" scoped>
+@import "@/styles/vuetify-preset-plus/light_theme/_variables.sass";
+
+.program-cert-block {
+  // max-width: 502px;
+  background: $neutral-100;
+  border: 1px solid #F2F2F7;
+  box-shadow: 0px 7px 20px rgba(88, 93, 106, 0.1);
+  border-radius: 10px;
+  padding: 10px;
+  .settings-block--content , .settings-block--chevron {
+    margin-top: 2px;
+  }
+}
+</style>
