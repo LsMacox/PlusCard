d1f1e039c55018687dead152bc98fb902d9c03f4
Legato <legatokun@gmail.com>
2020-09-30 18:24:44 +0700

    remove debug code

--- src/views/auth/registration/index.vue
+++ src/views/auth/registration/index.vue
@@ -152,9 +152,7 @@
           >
             <v-icon left>
               $iconify_feather-users
-            </v-icon>   
-             <v-icon      
-      > icon-warning-outline</v-icon>         
+            </v-icon>                       
             Создать аккаунт
           </v-btn>
         </div>
