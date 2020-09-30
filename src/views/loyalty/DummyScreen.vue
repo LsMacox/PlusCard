96bfddd6b0e5326dd8b08a3e3f2083aac266acd3
DmitriyAnokhin <hunter9@bk.ru>
2020-09-30 20:17:56 +0700

    fix loyalty

---
+++ src/views/loyalty/DummyScreen.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="dummy-screen">
+    <v-img
+      class="dummy-screen-img"
+      src="@//assets/png/loyalty-dummy.png"
+    />
+    <div class="dummy-screen-header title-s-bold">
+      Здесь пока ничего нет :’(
+    </div>
+    <div class="dummy-screen-text body-m-regular">
+      Здесь отобрается информация по операциям програмы лояльности. По текущим настройкам фильтра или периода данных нет.
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style lang="scss" scoped>
+.dummy-screen {
+  margin: 100px 0;
+
+  .dummy-screen-img {
+    margin: 0 auto;
+    width: 186px;
+    height: 156px;
+  }
+
+  .dummy-screen-header {
+    margin: 34px auto 0 auto;
+    max-width: 500px;
+    text-align: center;
+  }
+
+  .dummy-screen-text {
+    margin: 12px auto 0 auto;
+    max-width: 500px;
+    text-align: center;
+  }
+}
+</style>
--- src/views/loyalty/index.vue
+++ src/views/loyalty/index.vue
@@ -1,10 +1,14 @@
 <template>
   <div
+    v-if="programs.length"
     class="loyalty-block"
   >
     <toolbar />
     <widget-line />
     <app-table />
+  </div>
+  <div v-else>
+    <dummy-screen />
   </div>
 </template>
 
@@ -12,12 +16,14 @@
   import Toolbar from '@/views/loyalty/report/Toolbar'
   import WidgetLine from '@/views/loyalty/report/widget/index'
   import AppTable from '@/views/loyalty/report/table/index'
+  import DummyScreen from '@/views/loyalty/DummyScreen'
 
   export default {
     components: {
       Toolbar,
       WidgetLine,
       AppTable,
+      DummyScreen,
     },
     data () {
       return {
@@ -31,6 +37,9 @@
       },
       loadingApp () {
         return this.$store.getters['template/shared/loadingApp']
+      },
+      programs () {
+        return this.$store.getters['company/program/programs']
       },
       program () {
         return this.$store.getters['company/program/program']
@@ -67,24 +76,26 @@
       async fetchData () {
         console.log('fetchData ALL')
         try {
-          this.loading = true
-          const widget = {
-            program_id: this.program.id,
-            start_period: this.period.start,
-            end_period: this.period.end,
-            filter: this.filter,
-          }
-          // console.log('load_data', widget)
-          // console.log('load_data', this.period)
+          if (this.program.id) {
+            this.loading = true
+            const widget = {
+              program_id: this.program.id,
+              start_period: this.period.start,
+              end_period: this.period.end,
+              filter: this.filter,
+            }
+            // console.log('load_data', widget)
+            // console.log('load_data', this.period)
 
-          await this.$store.dispatch('company/bonus_resources/GetActiveShortList', this.program.id)
-          await this.$store.dispatch('company/bonus_units/loadBonusUnits', this.program.id)
-          //
-          await this.$store.dispatch('widget/bonusClients/widget', widget)
-          await this.$store.dispatch('widget/operations/widget', widget)
-          await this.$store.dispatch('widget/operators/widget', widget)
-          await this.$store.dispatch('widget/bonuses/widget', widget)
-          // await this.$store.dispatch('widget/table/widget', widget)
+            await this.$store.dispatch('company/bonus_resources/GetActiveShortList', this.program.id)
+            await this.$store.dispatch('company/bonus_units/loadBonusUnits', this.program.id)
+            //
+            await this.$store.dispatch('widget/bonusClients/widget', widget)
+            await this.$store.dispatch('widget/operations/widget', widget)
+            await this.$store.dispatch('widget/operators/widget', widget)
+            await this.$store.dispatch('widget/bonuses/widget', widget)
+            // await this.$store.dispatch('widget/table/widget', widget)
+          }
         } finally {
           this.loading = false
         }
