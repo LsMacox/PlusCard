f5910c4f9e61a2b55dcdaadb6489de11c363b9bd
Legato <legatokun@gmail.com>
2020-10-13 12:31:10 +0700

    изменения

--- src/config/app.js
+++ src/config/app.js
@@ -1,7 +1,7 @@
 module.exports = Object.freeze({
     title: 'Plus Cabinet',
-    version: '2.0.3',
-    build: 301,
+    version: '2.0.4',
+    build: 301, // Server-API
     socketHost: process.env.VUE_APP_SOCKET_HOST || 'https://test-backend.pluscards.ru:6001',
     RECAPTCHA_SITE_KEY: process.env.VUE_APP_RECAPTCHA_SITE_KEY || '',
 })
