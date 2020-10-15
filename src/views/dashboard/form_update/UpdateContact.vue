9dbf4bf7611d652a30fbcd63167c5e5b64222686
Legato <legatokun@gmail.com>
2020-10-15 17:18:51 +0700

    fix socials

--- src/views/dashboard/form_update/UpdateContact.vue
+++ src/views/dashboard/form_update/UpdateContact.vue
@@ -81,7 +81,12 @@
     created () {
       console.log('this.programModel', this.programModel)
       const program = JSON.parse(JSON.stringify(this.programModel))
-      // program.socials = JSON.parse(this.programModel.socials)
+      if (typeof this.programModel.socials === 'string') {
+        
+        program.socials = JSON.parse(this.programModel.socials)
+        console.log('program.socials', program.socials)
+      }
+      // 
       this.program = program
     },
     methods: {
