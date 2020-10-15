ce32e52bb8bdddc7fcd5987ac3a2deec298ddbe7
DmitriyAnokhin <hunter9@bk.ru>
2020-10-15 13:12:55 +0700

    new cabinet registration link

--- src/views/auth/password/forget/index.vue
+++ src/views/auth/password/forget/index.vue
@@ -101,6 +101,7 @@
 <script>
   import { mapGetters } from 'vuex'
   import BackButton from '@/views/auth/components/BackButton'
+  import { validEmail } from '@/utils/validate.js'
 
   export default {
     components: {
@@ -116,7 +117,7 @@
         visible1: false,
         emailRules: [
           v => !!v || 'E-mail обязателен',
-          v => /^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,10}$/.test(v) || 'E-mail неверного формата',
+          v => validEmail(v) || 'E-mail неверного формата',
         ],
         loading: false,
         emailSend: false,
