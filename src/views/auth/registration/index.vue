f52e89beb76752e0bee1dbc7593c4598aa7eb46b
DmitriyAnokhin <hunter9@bk.ru>
2020-10-15 12:43:44 +0700

    new cabinet registration link

--- src/views/auth/registration/index.vue
+++ src/views/auth/registration/index.vue
@@ -265,7 +265,7 @@
           device_token: this.device.token,
           device_type: this.device.type,
           recaptcha_token: recaptchaToken,
-          client: 'new',
+          client: 'dev',
         }
         console.log(user)
         try {
