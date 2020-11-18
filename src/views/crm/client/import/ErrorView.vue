7f2d68f008050518f063394459c0278dee1959b2
DmitriyAnokhin <hunter9@bk.ru>
2020-11-18 18:13:28 +0700

    import errors

---
+++ src/views/crm/client/import/ErrorView.vue
@@ -0,0 +1,112 @@
+<template>
+  <v-dialog
+    v-model="dialog"
+    max-width="600"
+  >
+    <v-card>
+      <div class="content">
+        <div class="header">
+          <div
+            v-if="type === 'error'"
+            class="body-m-regular header-item header-status-error"
+          >
+            <v-icon>$iconify_bx-bxs-error-alt</v-icon>
+            <span
+              class="header-text"
+            >{{ errors.length }} {{ declOfNum(errors.length, errorStr) }}
+            </span>
+          </div>
+          <div
+            v-if="type === 'warning'"
+            class="body-m-regular header-item header-status-warning"
+          >
+            <v-icon>$iconify_bx-bxs-error</v-icon>
+            <span
+              class="header-text"
+            >{{ errors.length }} {{ declOfNum(errors.length, warningStr) }}</span>
+          </div>
+        </div>
+        <div
+          v-for="(item, i) in errors"
+          :key="i"
+        >
+          {{ item }}
+        </div>
+        <div class="action">
+          <v-spacer />
+          <v-btn
+            color="secondary"
+            @click="close"
+          >
+            Закрыть
+          </v-btn>
+        </div>
+      </div>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+  import convertor from '@/mixins/convertor'
+
+  export default {
+    mixins: [convertor],
+    props: {
+      dialog: {
+        type: Boolean,
+        default: false,
+      },
+      errors: {
+        type: Array,
+        default: () => ([]),
+      },
+      type: {
+        type: String,
+        default: '',
+      },
+    },
+    data () {
+      return {
+        errorStr: ['ошибка', 'ошибки', 'ошибок'],
+        warningStr: ['предупреждение', 'предупреждения', 'предупреждений'],
+      }
+    },
+    methods: {
+      close () {
+        this.$emit('update:dialog', false)
+      },
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+  .header-item {
+    margin-bottom: 15px;
+    color: #f44336;
+    cursor: pointer;
+
+    .header-text {
+      position: relative;
+      top: -3px;
+      margin-left: 7px;
+    }
+  }
+
+  .header-status-error {
+    color: #f44336;
+  }
+
+  .header-status-warning {
+    color: #FF9800;
+  }
+
+  .content {
+    padding: 25px;
+  }
+
+  .action{
+    margin-top: 20px;
+    display: flex;
+    align-items: center;
+  }
+</style>
--- src/views/crm/client/import/StepTwo.vue
+++ src/views/crm/client/import/StepTwo.vue
@@ -10,11 +10,34 @@
       Выберите в таблице ниже к кому типу отнести импортируемые данные.<br>
       Обязательные ячейки: телефон или email.
     </div>
-    <div
-      class="body-m-regular"
-      style="margin-top: 12px;"
-    >
-      Всего загружено: {{ excel.length }} {{ declOfNum(excel.length, rowStr) }}
+    <div class="my-stepper-import--form-status">
+      <div
+        class="body-m-regular"
+        style="font-weight: 600;"
+      >
+        Предварительно загружено: {{ excel.length }} {{ declOfNum(excel.length, rowStr) }}
+      </div>
+      <div
+        v-if="errors.length"
+        class="body-m-regular my-stepper-import--form-status-error-item my-stepper-import--form-status-error"
+        @click="toErrorView('error')"
+      >
+        <v-icon>$iconify_bx-bxs-error-alt</v-icon>
+        <span
+          class="my-stepper-import--form-status-error-text"
+        >{{ errors.length }} {{ declOfNum(errors.length, errorStr) }}
+        </span>
+      </div>
+      <div
+        v-if="warnings.length"
+        class="body-m-regular my-stepper-import--form-status-error-item my-stepper-import--form-status-warning"
+        @click="toErrorView('warning')"
+      >
+        <v-icon>$iconify_bx-bxs-error</v-icon>
+        <span
+          class="my-stepper-import--form-status-error-text"
+        >{{ warnings.length }} {{ declOfNum(warnings.length, warningStr) }}</span>
+      </div>
     </div>
     <div class="my-stepper-import--form-content">
       <div class="my-stepper-import--step-two">
@@ -87,13 +110,25 @@
         Загрузить базу
       </v-btn>
     </div>
+    <error-view
+      v-if="dialogError"
+      :dialog.sync="dialogError"
+      :errors="viewErr"
+      :type="viewType"
+    />
   </div>
 </template>
 
 <script>
   import convertor from '@/mixins/convertor'
+  import { emailV } from '@/plugins/validate-rules'
+  import { validPhone } from '@/utils/validate'
+  import ErrorView from '@/views/crm/client/import/ErrorView'
 
   export default {
+    components: {
+      ErrorView,
+    },
     mixins: [convertor],
     props: {
       step: {
@@ -118,8 +153,15 @@
           { value: 'middlename', text: 'Отчество', required: false },
         ],
         cols: [], // колонки excel по типам
+        errors: [], // ошибки
+        warnings: [], // предупреждения
+        viewErr: [], // просмотр ошибок
+        viewType: null,
         rowStr: ['строка', 'строки', 'строк'],
+        errorStr: ['ошибка', 'ошибки', 'ошибок'],
+        warningStr: ['предупреждение', 'предупреждения', 'предупреждений'],
         selectedExcel: [], // excel по выбранным колонкам
+        dialogError: false,
       }
     },
     computed: {
@@ -140,6 +182,8 @@
       },
       isValidExcel () {
         let reqCount = 0
+        let errors = []
+        let warnings = []
         const req = this.types.filter(item => item.required)
         if (req.length) {
           req.forEach(item => {
@@ -148,7 +192,26 @@
             }
           })
         }
-        if (reqCount) {
+        this.cols.forEach((type, i) => {
+          if (type) {
+            switch (type) {
+              case 'phone':
+                // errors.concat(this.checkingErrors(type, i))
+                errors = errors.concat(this.checkingErrors(type, i))
+                break
+              case 'email':
+                // errors.concat(this.checkingErrors(type, i))
+                errors = errors.concat(this.checkingErrors(type, i))
+                break
+              default:
+                warnings = warnings.concat(this.checkingWarnings(type, i))
+                break
+            }
+          }
+        })
+        this.errors = errors
+        this.warnings = warnings
+        if (reqCount && !errors.length) {
           return true
         }
         return false
@@ -166,6 +229,46 @@
             this.cols[index] = null
           }
         })
+      },
+      checkingErrors (type, i) {
+        const errors = []
+        this.excel.forEach((row) => {
+          const col = Object.keys(row)[i]
+          const rowNum = row.__rowNum__
+          const cell = row[col]
+          switch (type) {
+            case 'phone':
+              if (!cell) errors.push(`Строка ${(rowNum)}: Отсутствует номер телефона`)
+              if (cell && !validPhone(cell)) errors.push(`Строка ${(rowNum)}: Номер телефона неверного формата`)
+              break
+
+            case 'email':
+              if (!cell) errors.push(`Строка ${(rowNum)}: Отсутствует e-mail`)
+              if (cell && !emailV(cell)) errors.push(`Строка ${(rowNum)}: E-mail неверного формата`)
+              break
+          }
+        })
+        return errors
+      },
+      checkingWarnings (type, i) {
+        const warnings = []
+        this.excel.forEach((row) => {
+          const col = Object.keys(row)[i]
+          const rowNum = row.__rowNum__
+          const cell = row[col]
+          if (!cell) warnings.push(`Строка ${(rowNum)}: Пустая ячейка`)
+        })
+        return warnings
+      },
+      toErrorView (type) {
+        if (type === 'error') {
+          this.viewErr = this.errors
+        }
+        if (type === 'warning') {
+          this.viewErr = this.warnings
+        }
+        this.viewType = type
+        this.dialogError = true
       },
       // подготовка excel
       getSelectedExcel () {
