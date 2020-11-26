1542593d777491aa9a91f71329cfe6e1acf4c7e9
Legato <legatokun@gmail.com>
2020-11-26 14:47:47 +0700

    ver

--- src/config/app.js
+++ src/config/app.js
@@ -1,6 +1,6 @@
 module.exports = Object.freeze({
     title: 'Plus Cabinet',
-    version: '2.9.8',
+    version: '2.9.9',
     build: 301, // Server-API
     socketHost: process.env.VUE_APP_SOCKET_HOST || 'https://test-backend.pluscards.ru:6001',
     fileStorage: process.env.VUE_APP_FILE_HOST || 'https://storage.yandexcloud.net/plusstorage/',
